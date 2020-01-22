529396ce0c30b932a17eed8ed48138a5da6a049b
github username <github email address>
2020-01-21 19:49:33 -0500

    added cli file, app dir

--- lib/app/cli_interface.rb
+++ lib/app/cli_interface.rb
@@ -4,6 +4,22 @@ class CliInterface
     end
 
     def sign_up
-        
+        name = nil
+        username = nil
+        password = nil
+
+        puts "Enter a your name:"
+        name = gets.chomp        
+        puts "Enter a username:"
+        username = gets.chomp
+        puts "Enter a password:"
+        password = gets.chomp
+
+        User.create(name: name, username: username, password: password)
+    end
+    
+    def prompt_to_login_or_signup
+        puts "Log in or Sign up"
+        gets.chomp 
     end
 end
