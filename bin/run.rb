410d9c68aad4bfbe512a0058d9f50775c0c6f140
myk245 <myk245@nyu.edu>
2020-01-22 10:12:08 -0500

    added exists? method

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,11 @@
 require_relative '../config/environment'
 
+app1 = CliInterface.new
+# app1.valid?('fefefe')
 
+# binding.pry
 
 puts "hello world"
+
+app1.greeting
+app1.sign_up
--- lib/app/cli_interface.rb
+++ lib/app/cli_interface.rb
@@ -8,18 +8,31 @@ class CliInterface
         username = nil
         password = nil
 
-        puts "Enter a your name:"
+        puts "Enter your name:"
         name = gets.chomp        
         puts "Enter a username:"
-        username = gets.chomp
+        username_input = gets.chomp
+        exists?(username_input) 
+        # call method to check if username exists 
         puts "Enter a password:"
         password = gets.chomp
 
-        User.create(name: name, username: username, password: password)
+        User.create(name: name, username: username_input, password: password)
     end
     
     def prompt_to_login_or_signup
         puts "Log in or Sign up"
         gets.chomp 
     end
+
+    def exists?(username_input)
+        # validates with database if that username exists or not
+        # returns true/false
+        if User.all.select{|user| user.username == username_input} == [] 
+            return false 
+        else
+            return true  
+        end 
+    end 
+
 end
