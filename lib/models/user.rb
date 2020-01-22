2923a630c4ba9ded682baa4eb47837b923525ca1
myk245 <myk245@nyu.edu>
2020-01-21 19:44:19 -0500

    added updated seed

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,8 +1,4 @@
 class User < ActiveRecord::Base
     has_many :reviews 
     has_many :coffee_shops, through: :reviews
-
-    def write_review(coffee_shop:, description:, rating:) 
-        review = Review.create(self.id, coffee_shop: coffee_shop, description: description,rating: rating)
-    end
 end
